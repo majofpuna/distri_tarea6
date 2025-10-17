034d58c061c5426abab97a43ce47390b756519ff
kevinfpuna <majofpuna@users.noreply.github.com>
2025-10-17 15:06:00 -0300

    agregar servicios

--- servidor-springboot/src/main/java/py/una/pol/sd/service/SucursalService.java
+++ servidor-springboot/src/main/java/py/una/pol/sd/service/SucursalService.java
@@ -1,20 +1,4 @@
 package py.una.pol.sd.service;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import py.una.pol.sd.model.Sucursal;
-import py.una.pol.sd.repository.SucursalRepository;
-
-@Service
-public class SucursalService {
-
-    @Autowired
-    SucursalRepository repository;
-
-    public List<Sucursal> listar() {
-        return repository.findAll();
-    }
-}
+import java.util.List; import org.springframework.beans.factory.annotation.Autowired; import org.springframework.stereotype.Service;
+import py.una.pol.sd.model.Sucursal; import py.una.pol.sd.repository.SucursalRepository;
+@Service public class SucursalService { @Autowired SucursalRepository repo; public List<Sucursal> listar(){ return repo.findAll(); } }
