be219947a1f236ee42644460d80b12d971cb9a52
amandacaceres <kevinfpuna@users.noreply.github.com>
2025-10-17 13:38:00 -0300

    agregar modelos de datos

--- servidor-springboot/src/main/java/py/una/pol/sd/model/Beneficiario.java
+++ servidor-springboot/src/main/java/py/una/pol/sd/model/Beneficiario.java
@@ -1,57 +1,9 @@
 package py.una.pol.sd.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "beneficiarios")
+import javax.persistence.*;
+@Entity @Table(name = "beneficiarios")
 public class Beneficiario {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    
-    private String nombre;
-    private String cuenta;
-
-    protected Beneficiario() {}
-
-    public Beneficiario(String nombre, String cuenta) {
-        this.nombre = nombre;
-        this.cuenta = cuenta;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-            "Beneficiario[id=%d, nombre='%s', cuenta='%s']",
-            id, nombre, cuenta);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getCuenta() {
-        return cuenta;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setCuenta(String cuenta) {
-        this.cuenta = cuenta;
-    }
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Integer id;
+  private String nombre; private String cuenta;
+  public Integer getId(){return id;} public String getNombre(){return nombre;} public String getCuenta(){return cuenta;}
+  public void setId(Integer id){this.id=id;} public void setNombre(String n){this.nombre=n;} public void setCuenta(String c){this.cuenta=c;}
 }
--- servidor-springboot/src/main/java/py/una/pol/sd/model/Sucursal.java
+++ servidor-springboot/src/main/java/py/una/pol/sd/model/Sucursal.java
@@ -1,57 +1,9 @@
 package py.una.pol.sd.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "sucursales")
+import javax.persistence.*;
+@Entity @Table(name = "sucursales")
 public class Sucursal {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    
-    private String nombre;
-    private String ciudad;
-
-    protected Sucursal() {}
-
-    public Sucursal(String nombre, String ciudad) {
-        this.nombre = nombre;
-        this.ciudad = ciudad;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-            "Sucursal[id=%d, nombre='%s', ciudad='%s']",
-            id, nombre, ciudad);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Integer id;
+  private String nombre; private String ciudad;
+  public Integer getId(){return id;} public String getNombre(){return nombre;} public String getCiudad(){return ciudad;}
+  public void setId(Integer id){this.id=id;} public void setNombre(String n){this.nombre=n;} public void setCiudad(String c){this.ciudad=c;}
 }
