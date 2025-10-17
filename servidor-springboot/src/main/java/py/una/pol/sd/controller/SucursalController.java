be706fc766e5322d3571810150cfd0c660a34b32
amandacaceres <kevinfpuna@users.noreply.github.com>
2025-10-17 16:41:00 -0300

    agregar controladores REST

--- servidor-springboot/src/main/java/py/una/pol/sd/controller/SucursalController.java
+++ servidor-springboot/src/main/java/py/una/pol/sd/controller/SucursalController.java
@@ -1,32 +1,9 @@
 package py.una.pol.sd.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import py.una.pol.sd.model.Sucursal;
-import py.una.pol.sd.service.SucursalService;
-
-@RestController
-@RequestMapping("/sucursales")
+import org.springframework.web.bind.annotation.*; import org.springframework.http.*; import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List; import py.una.pol.sd.service.SucursalService; import py.una.pol.sd.model.Sucursal;
+@RestController @RequestMapping("/sucursales")
 public class SucursalController {
-
-    @Autowired
-    SucursalService sucursalService;
-
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<List<Sucursal>> listar() {
-        try {
-            List<Sucursal> sucursales = sucursalService.listar();
-            return new ResponseEntity<>(sucursales, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+  @Autowired SucursalService service;
+  @GetMapping
+  public ResponseEntity<List<Sucursal>> listar(){ return new ResponseEntity<>(service.listar(), HttpStatus.OK); }
 }
